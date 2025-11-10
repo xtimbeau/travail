731a4d63ea5ba303139c7318a41c8fb3e70cb77f
Xavier Timbeau <xavier.timbeau@sciencespo.fr>
2025-11-10 19:26:16 +0100

    indépendants

--- france-melodi.R
+++ france-melodi.R
@@ -3,32 +3,34 @@ library(ofce)
 library(melodi)
 
 br <- tibble::tribble(
-  ~nace_r2, ~md,    ~mdhi,  ~mdhfi,  ~nace,
+  ~nace_r2, ~md,    ~mdhi,  ~mdhfi,  ~nace_13,
   "A",      TRUE,   TRUE,   TRUE,  "A",
-  "B_D_E",  TRUE,   TRUE,   TRUE,  "B_D_E",
-  "C10T12", TRUE,   TRUE,   TRUE,  "C",
-  "C13T15", TRUE,   TRUE,   TRUE,  "C",
-  "C16T18", TRUE,   TRUE,   TRUE,  "C",
-  "C19",    TRUE,   TRUE,   TRUE,  "C",
-  "C20",    TRUE,   TRUE,   TRUE,  "C",
-  "C21",    TRUE,   TRUE,   TRUE,  "C",
-  "C22_23", TRUE,   TRUE,   TRUE,  "C",
-  "C24_25", TRUE,   TRUE,   TRUE,  "C",
-  "C26T28", TRUE,   TRUE,   TRUE,  "C",
-  "C29_30", TRUE,   TRUE,   TRUE,  "C",
-  "C31T33", TRUE,   TRUE,   TRUE,  "C",
+  "B_D_E",  TRUE,   TRUE,   TRUE,  "BTE",
+  "C10T12", TRUE,   TRUE,   TRUE,  "BTE",
+  "C13T15", TRUE,   TRUE,   TRUE,  "BTE",
+  "C16T18", TRUE,   TRUE,   TRUE,  "BTE",
+  "C19",    TRUE,   TRUE,   TRUE,  "BTE",
+  "C20",    TRUE,   TRUE,   TRUE,  "BTE",
+  "C21",    TRUE,   TRUE,   TRUE,  "BTE",
+  "C22_23", TRUE,   TRUE,   TRUE,  "BTE",
+  "C24_25", TRUE,   TRUE,   TRUE,  "BTE",
+  "C26T28", TRUE,   TRUE,   TRUE,  "BTE",
+  "C29_30", TRUE,   TRUE,   TRUE,  "BTE",
+  "C31T33", TRUE,   TRUE,   TRUE,  "BTE",
   "F",      TRUE,   TRUE,   TRUE,  "F",
   "G",      TRUE,   TRUE,   TRUE,  "G",
   "H",      TRUE,   TRUE,   TRUE,  "H",
   "I",      TRUE,   TRUE,   TRUE,  "I",
   "J",      TRUE,   TRUE,   TRUE,  "J",
-  "K",      TRUE,   TRUE,   FALSE,   "K",
+  "K",      TRUE,   TRUE,   FALSE,  "K",
   "L",      TRUE,   FALSE,  TRUE,  "L",
   "M_N",    TRUE,   TRUE,   TRUE,  "M_N",
   "OTQ",    FALSE,  FALSE,  FALSE,  "OTQ",
   "R",      TRUE,   TRUE,   TRUE,  "R",
   "S",      TRUE,   TRUE,   TRUE,  "S" )
 
+br_alt <- c("_T", "OTQ", "K", "L")
+br_plus <- unique(c(br$nace_13, "_T"))
 codes <- vroom::vroom("ACTIVITY.codes.csv")
 colnames(codes) <- c("nace_r2", "fr", "en")
 codes <- codes |>
@@ -47,79 +49,6 @@ vahim <- source_data("vaq.r")$naa |>
   filter(champ %in% c("mdhim", "mdhi"), geo %in% pays) |>
   pivot_wider(names_from = champ, values_from = van) |>
   mutate(r = (mdhim-mdhi)/mdhi)
-
-# ggplot(vahim) +
-#   aes(x=time, y=r, color = geo, group = geo)+
-#   geom_line(layout = "fixed", color = "gray85") +
-#   geom_line() +
-#   facet_wrap(vars(geo)) +
-#   scale_color_pays("eurostat") +
-#   theme_ofce()
-
-uses <- source_data("uses.r")$u1610
-
-# uses |>
-#   mutate(r2 = CI_L/mdhi) |>
-#   drop_na() |>
-#   ggplot()+
-#   aes(x=time, y=r2, color = geo, group = geo)+
-#   geom_line(layout = "fixed", color = "gray85") +
-#   geom_line() +
-#   geom_line(aes(y=r), linetype = "11") +
-#   facet_wrap(vars(geo)) +
-#   scale_color_pays("eurostat") +
-#   theme_ofce()
-
-fr_tes_L <- melodi::get_all_data("DD_CNA_SUT") |>
-  filter(
-    STO == "P2", PRICES=="V", UNIT_MEASURE=="XDC", PRODUCT=="CPA_L",
-    ACTIVITY %in% br$nace_r2) |>
-  transmute(
-    time = ymd(TIME_PERIOD, truncated=2),
-    nace_r2 = ACTIVITY,
-    p2l = OBS_VALUE ) |>
-  arrange(time)
-
-fr_emp <- melodi::get_all_data("DD_CNA_BRANCHES") |>
-  filter(REF_SECTOR == "S1",
-         STO%in%c("SAL", "SELF", "EMP"),
-         UNIT_MEASURE == "PS",
-         TRANSFORMATION == "N",
-         COUNTERPART_AREA == "W2",
-         ACTIVITY %in% br$nace_r2) |>
-  transmute(time = ymd(TIME_PERIOD, truncated=2),
-            value = OBS_VALUE,
-            STO,
-            nace_r2 = ACTIVITY) |>
-  filter(time>="1978-01-01") |>
-  pivot_wider(names_from = STO, values_from = value) |>
-  mutate(
-    across(c(EMP, SELF, SAL), ~replace_na(.x, 0)) ) |>
-  arrange(time, nace_r2)
-
-fr_branches <- melodi::get_all_data("DD_CNA_BRANCHES") |>
-  filter(REF_SECTOR == "S1",
-         STO%in%c("B1G", "P51C", "D1", "D29X39", "D21X31", "B2A3G", "B2G"),
-         PRICES == "V",
-         UNIT_MEASURE == "XDC",
-         TRANSFORMATION == "N",
-         ACTIVITY %in% br$nace_r2) |>
-  transmute(time = ymd(TIME_PERIOD, truncated=2),
-            value = OBS_VALUE,
-            nace_r2 = ACTIVITY,
-            na_item = STO,
-            ae = ACCOUNTING_ENTRY,
-            ca = COUNTERPART_AREA) |>
-  filter(time>="1978-01-01") |>
-  arrange(time, na_item, nace_r2) |>
-  pivot_wider(names_from = c(ae, ca) , values_from = value) |>
-  rowwise() |>
-  mutate(value = coalesce(B_W0, B_W2, D_W2, D_W0)) |>
-  select(time, nace_r2, na_item, value) |>
-  pivot_wider(names_from = na_item, values_from = value) |>
-  rename_with(tolower) |>
-  mutate(b3g = b2a3g - b2g) |>
-  left_join(fr_tes_L, by = c("nace_r2", "time"))
 
 men <- "DD_CNA_CONSO_MENAGES_PRODUITS" |>
   melodi::get_all_data() |>
@@ -142,9 +71,135 @@ men <- "DD_CNA_CONSO_MENAGES_PRODUITS" |>
     lymenree = L68R1A+L68R1B) |>
   arrange(desc(time))
 
+men2 <- "DD_CNA_SUT" |>
+  melodi::get_all_data() |>
+  filter(STO=="P3",
+         PRODUCT == "CPA_L",
+         REF_SECTOR == "S1") |>
+  arrange(TIME_PERIOD) |>
+  transmute(
+    time = ymd(TIME_PERIOD, truncated=2),
+    p3l = OBS_VALUE) |>
+  arrange(desc(time))
+
+fr_tes_L <- "DD_CNA_SUT" |>
+  melodi::get_all_data() |>
+  filter(
+    STO == "P2", PRICES=="V", UNIT_MEASURE=="XDC", PRODUCT=="CPA_L",
+    ACTIVITY %in% br_alt) |>
+  transmute(
+    time = ymd(TIME_PERIOD, truncated=2),
+    nace_r2 = ACTIVITY,
+    p2l  = OBS_VALUE ) |>
+  arrange(time)
+
+fr_emp <- melodi::get_all_data("DD_CNA_BRANCHES") |>
+  filter(REF_SECTOR == "S1",
+         STO%in%c("SAL", "SELF", "EMP"),
+         UNIT_MEASURE == "PS",
+         TRANSFORMATION == "N",
+         COUNTERPART_AREA == "W2",
+         ACTIVITY %in% br_plus) |>
+  transmute(time = ymd(TIME_PERIOD, truncated=2),
+            value = OBS_VALUE,
+            STO,
+            nace_r2 = ACTIVITY) |>
+  # filter(time>="1978-01-01") |>
+  pivot_wider(names_from = STO, values_from = value) |>
+  mutate(
+    across(c(EMP, SELF, SAL), ~replace_na(.x, 0)) ) |>
+  arrange(time, nace_r2)
+
+fr_b23g <- melodi::get_all_data("DD_CNA_BRANCHES") |>
+  filter(REF_SECTOR == "S1",
+         STO%in%c("B2A3G", "B2G"),
+         PRICES == "V",
+         UNIT_MEASURE == "XDC",
+         TRANSFORMATION == "N",
+         COUNTERPART_AREA == "W0",
+         ACCOUNTING_ENTRY == "B",
+         ACTIVITY %in% br_plus) |>
+  transmute(time = ymd(TIME_PERIOD, truncated=2),
+            value = OBS_VALUE,
+            STO,
+            nace_r2 = ACTIVITY) |>
+  pivot_wider(names_from = STO, values_from = value) |>
+  mutate(b3g = B2A3G - B2G) |>
+  arrange(time, nace_r2)
+
+fr_d1 <- melodi::get_all_data("DD_CNA_BRANCHES") |>
+  filter(REF_SECTOR == "S1",
+         STO%in%c("D1"),
+         PRICES == "V",
+         UNIT_MEASURE == "XDC",
+         TRANSFORMATION == "N",
+         COUNTERPART_AREA == "W0",
+         ACCOUNTING_ENTRY == "D",
+         ACTIVITY %in% br_plus) |>
+  transmute(time = ymd(TIME_PERIOD, truncated=2),
+            value = OBS_VALUE,
+            STO,
+            nace_r2 = ACTIVITY) |>
+  pivot_wider(names_from = STO, values_from = value) |>
+  arrange(time, nace_r2)
+
+fr_ind <- fr_emp |>
+  full_join(fr_b23g, by = c("time", "nace_r2")) |>
+  full_join(fr_d1, by = c("time", "nace_r2")) |>
+  group_by(time) |>
+  mutate(
+    rb3g = b3g/b3g[nace_r2=="_T"],
+    rpc = (b3g/SELF)/(D1[nace_r2=="_T"]/SAL[nace_r2=="_T"]),
+    rpc = ifelse(rpc<0, NA, rpc)) |>
+  group_by(nace_r2) |>
+  fill(rpc, .direction="downup") |>
+  group_by(time) |>
+  mutate(b3g = rpc * D1[nace_r2=="_T"]/SAL[nace_r2=="_T"] * SELF,
+         vv = sum(b3g[nace_r2!="_T"])/b3g[nace_r2 == "_T"]) |>
+  mutate(b3g = ifelse(nace_r2=="_T", b3g, b3g/vv)) |>
+  rename_with(tolower) |>
+  ungroup()
+
+fr_branches <- melodi::get_all_data("DD_CNA_BRANCHES") |>
+  filter(REF_SECTOR == "S1",
+         STO%in%c("B1G", "P51C", "D1", "D29X39", "B2A3G", "B2G"),
+         PRICES == "V",
+         UNIT_MEASURE == "XDC",
+         TRANSFORMATION == "N",
+         ACTIVITY %in% br_alt) |>
+  transmute(time = ymd(TIME_PERIOD, truncated=2),
+            value = OBS_VALUE,
+            nace_r2 = ACTIVITY,
+            na_item = STO,
+            ca = COUNTERPART_AREA,
+            ae = ACCOUNTING_ENTRY) |>
+  arrange(time, na_item, nace_r2) |>
+  pivot_wider(names_from = c(ae, ca) , values_from = value) |>
+  rowwise() |>
+  mutate(value = coalesce(B_W0, B_W2, D_W0, D_W2)) |>
+  select(time, nace_r2, na_item, value) |>
+  pivot_wider(names_from = na_item, values_from = value) |>
+  rename_with(tolower) |>
+  mutate(b3g = b2a3g - b2g) |>
+  left_join(fr_tes_L, by = c("nace_r2", "time"))
+
+# On prolonge pour 2024 b2g sur la valeur "total" (_T) et d29x30 sur le ratio b1g
+# Sur le passé on prolonge p51c et d29x39 sur b1g et b2g sur NSAL/SAL
+
+fr_branches_aug <- fr_branches |>
+  arrange(time) |>
+  select(-b3g) |>
+  left_join(fr_ind |> select(time, nace_r2, b3g, sal, self), by = c("nace_r2", "time")) |>
+  mutate(rp51c = p51c / b1g,
+         rd29x39 = d29x39 / b1g) |>
+  group_by(nace_r2) |>
+  fill(rp51c, rd29x39, .direction = "updown") |>
+  rename_with(tolower) |>
+  ungroup()
+
 assets <- "DD_CNA_PATRIMOINE_BRANCHES" |>
   melodi::get_all_data() |>
-  filter(ACTIVITY %in% br$nace_r2,
+  filter(ACTIVITY %in% br_alt,
          STO == "LE", PRICES=="U", COUNTERPART_AREA == "W0",
          INSTR_ASSET %in% c("N1N", "N111N")) |>
   transmute(
@@ -156,24 +211,21 @@ assets <- "DD_CNA_PATRIMOINE_BRANCHES" |>
   pivot_wider(names_from = asset, values_from = value) |>
   mutate(across(c(N1N, N111N), ~replace_na(.x, 0)))
 
-melodi <- fr_branches |>
-  left_join(fr_emp, by = c("time", "nace_r2")) |>
+melodi <- fr_branches_aug |>
   left_join(assets, by = c("time", "nace_r2")) |>
   left_join(men, by = c("time", "nace_r2")) |>
   left_join(br, by = "nace_r2") |>
   rename_with(tolower) |>
   mutate(
-    d29x39 = replace_na(d29x39, 0)  ) |>
-  mutate(
     vab = b1g,
     van = b1g - p51c,
     psal = (d1 * (1 + self/sal)) / van,
-    psalm = (d1 + b3g*0.8) / van,
+    psalm = (d1 + b3g*0.88) / van,
     p2lf = p2l[time=="1980-01-01"]/van[time=="1980-01-01"] * van,
     tp = (van - d1 * (1 + self/sal) - d29x39) / van,
-    tpm = (van - d1 - b3g * 0.8 - d29x39) / van,
+    tpm = (van - d1 - b3g * 0.88 - d29x39) / van,
     rb = (van - d1 * (1 + self/sal) - d29x39) / n1n,
-    rpm = (van - d1 - b3g * 0.8  - d29x39) / n1n)
+    rpm = (van - d1 - b3g * 0.88  - d29x39) / n1n)
 
 dvalL <- melodi |>
   group_by(time) |>
@@ -191,9 +243,9 @@ melodi <- melodi |>
     rb2 = (van + dva - d1 * (1 + self/sal) - d29x39) / n1n)
 
 melodi2 <- melodi |>
-  group_by(time, nace) |>
+  group_by(time, nace_13) |>
   summarize(
-    across(c(b1g, b3g, d1, d29x39, p51c, p2l, p2lf, emp, self, sal, n111n, n1n, lymen, dva), sum),
+    across(c(b1g, b3g, d1, d29x39, p51c, p2l, p2lf, self, sal, n111n, n1n, lymen, dva), sum),
     across(c(md, mdhi), first),
     .groups = "drop") |>
   mutate(
@@ -207,7 +259,7 @@ melodi2 <- melodi |>
     rpm = (van - d1 - b3g * 0.8  - d29x39) / n1n,
     rb2 = (van + dva - d1 * (1 + self/sal) - d29x39) / n1n)
 
-ssi <- melodi2 |> filter(time == "2024-01-01",md) |> select(time, van, nace, md) |> mutate(van=van/sum(van))
+ssi <- melodi2 |> filter(time == "2024-01-01",md) |> select(time, van, nace_13, md) |> mutate(van=van/sum(van))
 
 ccf_ei <- melodi::get_all_data("DD_CNA_AGREGATS") |>
   filter(REF_SECTOR == "S14AA", STO %in% c("B3G", "P51C", "B3N"), PRICES=="V") |>
@@ -222,16 +274,16 @@ ccf_ei <- melodi::get_all_data("DD_CNA_AGREGATS") |>
 melodi_m <- melodi |>
   group_by(time) |>
   summarize(
-    across(c(b1g, b3g, d1, d29x39, p51c, p2l, p2lf, emp, self, sal, n111n, n1n, lymen, van, vab, dva),
-           ~sum(.x[md]), .names = "{.col}_md" ),
-    across(c(b1g, b3g, d1, d29x39, p51c, p2l, p2lf, emp, self, sal, n111n, n1n, lymen, van, vab, dva),
-           ~sum(.x[mdhi]), .names = "{.col}_mdhi" ),
-    across(c(b1g, b3g, d1, d29x39, p51c, p2l, p2lf, emp, self, sal, n111n, n1n, lymen, van, vab, dva),
-           ~sum(.x[mdhi&mdhfi]), .names = "{.col}_mdhifi" ),
-    across(c(b1g, b3g, d1, d29x39, p51c, p2l, p2lf, emp, self, sal, n111n, n1n, lymen, van, vab, dva),
-           ~sum(.x[mdhfi]), .names = "{.col}_mdhfi" ),
-    across(c(b1g, b3g, d1, d29x39, p51c, p2l, p2lf, emp, self, sal, n111n, n1n, lymen, van, vab, dva),
-           ~sum(.x), .names = "{.col}_tb" )) |>
+    across(c(b1g, b3g, d1, d29x39, p51c, p2l, p2lf, self, sal, n111n, n1n, lymen, van, vab, dva),
+           ~.x[nace_r2=="_T"]-.x[nace_r2=="OTQ"], .names = "{.col}_md" ),
+    across(c(b1g, b3g, d1, d29x39, p51c, p2l, p2lf, self, sal, n111n, n1n, lymen, van, vab, dva),
+           ~.x[nace_r2=="_T"]-.x[nace_r2=="OTQ"]-.x[nace_r2=="L"], .names = "{.col}_mdhi" ),
+    across(c(b1g, b3g, d1, d29x39, p51c, p2l, p2lf, self, sal, n111n, n1n, lymen, van, vab, dva),
+           ~.x[nace_r2=="_T"]-.x[nace_r2=="OTQ"]-.x[nace_r2=="L"]-.x[nace_r2=="K"], .names = "{.col}_mdhifi" ),
+    across(c(b1g, b3g, d1, d29x39, p51c, p2l, p2lf, self, sal, n111n, n1n, lymen, van, vab, dva),
+           ~.x[nace_r2=="_T"]-.x[nace_r2=="OTQ"]-.x[nace_r2=="K"], .names = "{.col}_mdhfi" ),
+    across(c(b1g, b3g, d1, d29x39, p51c, p2l, p2lf, self, sal, n111n, n1n, lymen, van, vab, dva),
+           ~.x[nace_r2=="_T"], .names = "{.col}_tb" ) ) |>
   pivot_longer(cols = ends_with(c("md", "mdhi", "mdhifi", "mdhfi", "tb"))) |>
   separate(name, sep="_", into = c("var", "champ")) |>
   pivot_wider(names_from = var, values_from = value) |>
@@ -247,7 +299,7 @@ melodi_m <- melodi |>
     rb2 = (van + dva - msa - d29x39) / n1n,
     psalm = msam / van,
     tp = (van - msa - d29x39) / van,
-    rb = (van - msam - d29x39) / n1n,
+    rbm = (van - msam - d29x39) / n1n,
     rb2 = (van + dva - msam - d29x39) / n1n)
 
 return(list(full = melodi, a20 = melodi2, aggr = melodi_m))
